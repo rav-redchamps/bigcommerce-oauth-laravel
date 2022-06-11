add814df20a870bc5937947f521a0adf5e6ae0a0
Hardik Modha (MadBoy Developers) <hardik@madboydevelopers.com>
2022-06-11 19:44:33 +0530

    feat: multi-user without role added

--- src/Http/Controllers/BigLoadController.php
+++ src/Http/Controllers/BigLoadController.php
@@ -2,11 +2,15 @@
 
 namespace MadBoy\BigCommerceAuth\Http\Controllers;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use MadBoy\BigCommerceAuth\Facades\BigCommerceAuth;
 use MadBoy\BigCommerceAuth\Models\Store;
@@ -44,15 +48,56 @@ class BigLoadController extends Controller
             $user = $this->getUserModelClass()::query()
                 ->where('email', $signed_payload['user']['email'])
                 ->first();
-            if ($user) {
-                Auth::login($user);
-                BigCommerceAuth::setStoreHash($signed_payload['store_hash']);
-                $store = $this->getStoreModelClass()::query()->where('hash', $signed_payload['store_hash'])->first();
-                BigCommerceAuth::callLoadCallback($user, $store);
-                return true;
+            if (!$user) {
+                $user = $this->saveUserIfNotExist($signed_payload['user']['email']);
+                if (!$user) {
+                    return false;
+                }
+                $store = $this->getStoreModelClass()::query()
+                    ->where('hash', $signed_payload['store_hash'])
+                    ->first();
+                if (!$store) {
+                    return false;
+                }
+                if (!$this->assignUserToStore($user->id, $store->id)) {
+                    return false;
+                }
             }
+            Auth::login($user);
+            BigCommerceAuth::setStoreHash($signed_payload['store_hash']);
+            $store = $this->getStoreModelClass()::query()
+                ->where('hash', $signed_payload['store_hash'])
+                ->first();
+            BigCommerceAuth::callLoadCallback($user, $store);
+            return true;
         }
         return false;
+    }
+
+    /**
+     * @param $email
+     * @return Model|Builder|Authenticatable
+     */
+    private function saveUserIfNotExist($email)
+    {
+        return $this->getUserModelClass()::query()->firstOrCreate([
+            'email' => $email
+        ]);
+    }
+
+    private function assignUserToStore($user_id, $store_id): bool
+    {
+        $store_has_users = Config::get('bigcommerce-auth.tables.store_has_users');
+        if (DB::table($store_has_users)
+            ->where('store_id', $store_id)
+            ->where('user_id', $user_id)
+            ->exists())
+            return true;
+
+        return DB::table($store_has_users)->insert([
+            'store_id' => $store_id,
+            'user_id' => $user_id,
+        ]);
     }
 
     private function getUserModelClass(): string
